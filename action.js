3fe56e4fc9c2d35918bae2f5e10b3634a1eb781f
Xgui4 Studio <134389196+xgui4@users.noreply.github.com>
2026-01-06 15:07:33 -0500

    add wip actions

--- action.js
+++ action.js
@@ -1,13 +1,25 @@
-let nav  = document.getElementsByTagName('nav')[0]
+let nav = document.getElementsByTagName('nav')[0];
+if (nav) {
+    let nav_div = nav.getElementsByTagName("div")[0];
+    if (nav_div) {
+        let nav_link = document.createElement('a');
 
-let nav_div = nav.getElementsByTagName("div")[0]
+        Object.assign(nav_link, {
+            className: "nav_link",
+            href: "#",
+            textContent: "Addon Page"
+        });
 
-let nav_link = document.createElement('a');
+        // Fix: Correct event listener syntax
+        nav_link.addEventListener("click", function(event) {
+            event.preventDefault();
+            go_to_link("");
+        });
 
-Object.assign(nav_link.className = "nav_link")
+        nav_div.append(nav_link);
+    }
+}
 
-Object.assign(nav_link.href = " resource:///@xgui4-addon/addon-page.html")
-
-Object.assign(nav_link.textContent = "Addon Page")
-
-nav_div.append(nav_link)
+function go_to_link(link) {
+    alert("Not implemented yet");
+}   
